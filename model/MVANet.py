f4f24bd03d092fe720bb418785880df55cf3244e
Karol <karol.kulawiec@spectre.solutions>
2024-12-20 13:44:34 +0100

    refactor model

--- model/MVANet.py
+++ model/MVANet.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn.functional as F
 from einops import rearrange
 from torch import nn
-from .SwinTransformer import SwinB
+from .SwinTransformer import SwinB, SwinT
 
 
 def get_activation_fn(activation):
@@ -57,31 +57,31 @@ class PositionEmbeddingSine:
         if scale is None:
             scale = 2 * math.pi
         self.scale = scale
-        self.dim_t = torch.arange(0, self.num_pos_feats, dtype=torch.float32, device='cuda') 
+        self.dim_t = torch.arange(0, self.num_pos_feats, dtype=torch.float32, device='cuda')
 
     def __call__(self, b, h, w):
         mask = torch.zeros([b, h, w], dtype=torch.bool, device='cuda')
         assert mask is not None
         not_mask = ~mask
-        y_embed = not_mask.cumsum(dim=1, dtype=torch.float32)  
-        x_embed = not_mask.cumsum(dim=2, dtype=torch.float32)  
+        y_embed = not_mask.cumsum(dim=1, dtype=torch.float32)
+        x_embed = not_mask.cumsum(dim=2, dtype=torch.float32)
         if self.normalize:
             eps = 1e-6
             y_embed = ((y_embed - 0.5) / (y_embed[:, -1:, :] + eps) * self.scale).cuda()
             x_embed = ((x_embed - 0.5) / (x_embed[:, :, -1:] + eps) * self.scale).cuda()
 
-        dim_t = self.temperature ** (2 * (self.dim_t // 2) / self.num_pos_feats) 
+        dim_t = self.temperature ** (2 * (self.dim_t // 2) / self.num_pos_feats)
 
-        pos_x = x_embed[:, :, :, None] / dim_t 
-        pos_y = y_embed[:, :, :, None] / dim_t  
+        pos_x = x_embed[:, :, :, None] / dim_t
+        pos_y = y_embed[:, :, :, None] / dim_t
         pos_x = torch.stack((pos_x[:, :, :, 0::2].sin(), pos_x[:, :, :, 1::2].cos()), dim=4).flatten(
-            3)  
+            3)
         pos_y = torch.stack((pos_y[:, :, :, 0::2].sin(), pos_y[:, :, :, 1::2].cos()), dim=4).flatten(
-            3)  
-        return torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2) 
+            3)
+        return torch.cat((pos_y, pos_x), dim=3).permute(0, 3, 1, 2)
 
 
-class MCLM(nn.Module):  
+class MCLM(nn.Module):
     def __init__(self, d_model, num_heads, pool_ratios=[1, 4, 8]):
         super(MCLM, self).__init__()
         self.attention = nn.ModuleList([
@@ -112,10 +112,10 @@ class MCLM(nn.Module):
         l: 4,c,h,w
         g: 1,c,h,w
         """
-        b, c, h, w = l.size() 
+        b, c, h, w = l.size()
         # 4,c,h,w -> 1,c,2h,2w
         concated_locs = rearrange(l, '(hg wg b) c h w -> b c (hg h) (wg w)', hg=2, wg=2)
-        
+
         pools = []
         for pool_ratio in self.pool_ratios:
              # b,c,h,w
@@ -151,13 +151,13 @@ class MCLM(nn.Module):
         l_hw_b_c = l_hw_b_c + self.dropout1(outputs_re)
         l_hw_b_c = self.norm1(l_hw_b_c)
         l_hw_b_c = l_hw_b_c + self.dropout2(self.linear4(self.dropout(self.activation(self.linear3(l_hw_b_c)).clone())))
-        l_hw_b_c = self.norm2(l_hw_b_c)  
+        l_hw_b_c = self.norm2(l_hw_b_c)
 
         l = torch.cat((l_hw_b_c, g_hw_b_c), 1)  # hw,b(5),c
         return rearrange(l, "(h w) b c -> b c h w", h=h, w=w)  ## (5,c,h*w)
 
 
-class inf_MCLM(nn.Module):  
+class inf_MCLM(nn.Module):
     def __init__(self, d_model, num_heads, pool_ratios=[1, 4, 8]):
         super(inf_MCLM, self).__init__()
         self.attention = nn.ModuleList([
@@ -188,7 +188,7 @@ class inf_MCLM(nn.Module):
         l: 4,c,h,w
         g: 1,c,h,w
         """
-        b, c, h, w = l.size() 
+        b, c, h, w = l.size()
         # 4,c,h,w -> 1,c,2h,2w
         concated_locs = rearrange(l, '(hg wg b) c h w -> b c (hg h) (wg w)', hg=2, wg=2)
         self.p_poses = []
@@ -227,7 +227,7 @@ class inf_MCLM(nn.Module):
         l_hw_b_c = l_hw_b_c + self.dropout1(outputs_re)
         l_hw_b_c = self.norm1(l_hw_b_c)
         l_hw_b_c = l_hw_b_c + self.dropout2(self.linear4(self.dropout(self.activation(self.linear3(l_hw_b_c)).clone())))
-        l_hw_b_c = self.norm2(l_hw_b_c)  
+        l_hw_b_c = self.norm2(l_hw_b_c)
 
         l = torch.cat((l_hw_b_c, g_hw_b_c), 1)  # hw,b(5),c
         return rearrange(l, "(h w) b c -> b c h w", h=h, w=w)  ## (5,c,h*w)
@@ -256,7 +256,7 @@ class MCRM(nn.Module):
         self.pool_ratios = pool_ratios
         self.positional_encoding = PositionEmbeddingSine(num_pos_feats=d_model // 2, normalize=True)
     def forward(self, x):
-        b, c, h, w = x.size()  
+        b, c, h, w = x.size()
         loc, glb = x.split([4, 1], dim=0)  # 4,c,h,w; 1,c,h,w
         # b(4),c,h,w
         patched_glb = rearrange(glb, 'b c (hg h) (wg w) -> (hg wg b) c h w', hg=2, wg=2)
@@ -279,7 +279,7 @@ class MCRM(nn.Module):
             v = pools[i]
             k = v
             outputs.append(self.attention[i](q, k, v)[0])
-        outputs = torch.cat(outputs, 1)  
+        outputs = torch.cat(outputs, 1)
         src = loc.view(4, c, -1).permute(2, 0, 1) + self.dropout1(outputs)
         src = self.norm1(src)
         src = src + self.dropout2(self.linear4(self.dropout(self.activation(self.linear3(src)).clone())))
@@ -313,7 +313,7 @@ class inf_MCRM(nn.Module):
         self.pool_ratios = pool_ratios
         self.positional_encoding = PositionEmbeddingSine(num_pos_feats=d_model // 2, normalize=True)
     def forward(self, x):
-        b, c, h, w = x.size()  
+        b, c, h, w = x.size()
         loc, glb = x.split([4, 1], dim=0)  # 4,c,h,w; 1,c,h,w
         # b(4),c,h,w
         patched_glb = rearrange(glb, 'b c (hg h) (wg w) -> (hg wg b) c h w', hg=2, wg=2)
@@ -336,7 +336,7 @@ class inf_MCRM(nn.Module):
             v = pools[i]
             k = v
             outputs.append(self.attention[i](q, k, v)[0])
-        outputs = torch.cat(outputs, 1)  
+        outputs = torch.cat(outputs, 1)
         src = loc.view(4, c, -1).permute(2, 0, 1) + self.dropout1(outputs)
         src = self.norm1(src)
         src = src + self.dropout2(self.linear4(self.dropout(self.activation(self.linear3(src)).clone())))
@@ -347,7 +347,7 @@ class inf_MCRM(nn.Module):
         return torch.cat((src, glb), 0)
 
 # model for single-scale training
-class MVANet(nn.Module):  
+class MVANet(nn.Module):
     def __init__(self):
         super().__init__()
         self.backbone = SwinB(pretrained=True)
@@ -408,7 +408,7 @@ class MVANet(nn.Module):
         e5 = self.multifieldcrossatt(loc_e5, glb_e5)  # (4,128,16,16)
 
         e4, tokenattmap4 = self.dec_blk4(e4 + resize_as(e5, e4))
-        e4 = self.conv4(e4) 
+        e4 = self.conv4(e4)
         e3, tokenattmap3 = self.dec_blk3(e3 + resize_as(e4, e3))
         e3 = self.conv3(e3)
         e2, tokenattmap2 = self.dec_blk2(e2 + resize_as(e3, e2))
@@ -429,10 +429,10 @@ class MVANet(nn.Module):
         final_output = self.output(final_output)
         ####
         sideout5 = self.sideout5(e5).cuda()
-        sideout4 = self.sideout4(e4) 
-        sideout3 = self.sideout3(e3) 
-        sideout2 = self.sideout2(e2) 
-        sideout1 = self.sideout1(e1) 
+        sideout4 = self.sideout4(e4)
+        sideout3 = self.sideout3(e3)
+        sideout2 = self.sideout2(e2)
+        sideout1 = self.sideout1(e1)
         #######glb_sideouts ######
         glb5 = self.sideout5(glb_e5)
         glb4 = sideout4[-1,:,:,:].unsqueeze(0)
@@ -449,9 +449,9 @@ class MVANet(nn.Module):
             return sideout5, sideout4,sideout3,sideout2,sideout1,final_output, glb5, glb4, glb3, glb2, glb1,tokenattmap4, tokenattmap3,tokenattmap2,tokenattmap1
         else:
             return final_output
-    
+
 # model for multi-scale testing
-class inf_MVANet(nn.Module):  
+class inf_MVANet(nn.Module):
     def __init__(self):
         super().__init__()
         self.backbone = SwinB(pretrained=True)
@@ -498,13 +498,13 @@ class inf_MVANet(nn.Module):
         loc = image2patches(x)
         input = torch.cat((loc, glb), dim=0)
         feature = self.backbone(input)
-        e5 = self.output5(feature[4])  
-        e4 = self.output4(feature[3])  
-        e3 = self.output3(feature[2])  
-        e2 = self.output2(feature[1]) 
-        e1 = self.output1(feature[0])  
+        e5 = self.output5(feature[4])
+        e4 = self.output4(feature[3])
+        e3 = self.output3(feature[2])
+        e2 = self.output2(feature[1])
+        e1 = self.output1(feature[0])
         loc_e5, glb_e5 = e5.split([4, 1], dim=0)
-        e5_cat = self.multifieldcrossatt(loc_e5, glb_e5)  
+        e5_cat = self.multifieldcrossatt(loc_e5, glb_e5)
 
         e4 = self.conv4(self.dec_blk4(e4 + resize_as(e5_cat, e4)))
         e3 = self.conv3(self.dec_blk3(e3 + resize_as(e4, e3)))
@@ -516,7 +516,7 @@ class inf_MVANet(nn.Module):
         # add glb feat in
         output1_cat = output1_cat + resize_as(glb_e1, output1_cat)
         # merge
-        final_output = self.insmask_head(output1_cat) 
+        final_output = self.insmask_head(output1_cat)
         # shallow feature merge
         final_output = final_output + resize_as(shallow, final_output)
         final_output = self.upsample1(rescale_to(final_output))
@@ -524,5 +524,3 @@ class inf_MVANet(nn.Module):
         final_output = self.upsample2(final_output)
         final_output = self.output(final_output)
         return final_output
-
-
